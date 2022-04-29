bb9b11c77b19aaf30fc2a07bff65d7f4f2d6b943
lgy123123123 <77092167+lgy123123123@users.noreply.github.com>
2022-04-29 10:14:26 +0800

    Add files via upload

---
+++ LgyUtil.ModelCheck/StringTrimAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LgyUtil.ModelCheck
+{
+    /// <summary>
+    /// 将字符串进行Trim，放在class上，将class的所有字符串进行trim
+    /// 若属性有此特性，则只trim此类中打上次特性的字符串
+    /// 可以new的模型才可以使用
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Class,
+        AllowMultiple = false, Inherited = true)]
+    public class StringTrimAttribute : Attribute
+    {
+        /// <summary>
+        /// 不用进行trim
+        /// </summary>
+        public bool NotTrim { get; set; } = false;
+    }
+}
---
+++ LgyUtil.ModelCheck/StringTrimUtil.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace LgyUtil.ModelCheck
+{
+    public static class StringTrimUtil
+    {
+        /// <summary>
+        /// trim类的缓存
+        /// </summary>
+        static readonly ConcurrentDictionary<string, List<PropertyInfo>> dicTrimCache = new ConcurrentDictionary<string, List<PropertyInfo>>();
+        /// <summary>
+        /// 对所有标记StringTrimAttribute的字符串进行trim
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model"></param>
+        public static void TrimAll<T>(this T model) where T : class, new()
+        {
+            var t = model.GetType();
+            List<PropertyInfo> NeedTrimList;
+            if (!dicTrimCache.TryGetValue(t.FullName, out NeedTrimList))
+            {
+                var thisAttr = t.GetCustomAttribute<StringTrimAttribute>();
+                bool isTrimAll = thisAttr != null && !thisAttr.NotTrim;
+                var props = t.GetProperties().ToList().FindAll(p => p.PropertyType == typeof(string));
+                NeedTrimList = new List<PropertyInfo>();
+                foreach (var propInfo in props)
+                {
+                    var propAttr = propInfo.GetCustomAttribute<StringTrimAttribute>();
+                    if (isTrimAll || propAttr != null)
+                    {
+                        if (propAttr != null && propAttr.NotTrim)//标记nottrim的不要
+                            continue;
+                        NeedTrimList.Add(propInfo);
+                    }
+                }
+                NeedTrimList.TrimExcess();
+                dicTrimCache.TryAdd(t.FullName, NeedTrimList);
+            }
+
+            if (NeedTrimList.Count == 0) return;
+            NeedTrimList.ForEach(p =>
+            {
+                object str = p.GetValue(model);
+                if (str != null)
+                {
+                    p.SetValue(model, (str as string).Trim());
+                }
+            });
+        }
+    }
+}
