52c12db1e90ea4386ede1e2e2900b245cf8e58f3
lgy <565493752@qq.com>
2025-05-14 10:33:11 +0800

    1、日志帮助类，增加输出为1行的设置，且给多线程访问写日志的线程增加了锁

--- LgyUtil/LogUtil.cs
+++ LgyUtil/LogUtil.cs
@@ -14,45 +14,62 @@ namespace LgyUtil
     public sealed class LogUtil
     {
         #region 公有方法和属性
+
+        /// <summary>
+        /// 每条日志，是否输出为1行，默认是false，带换行输出
+        /// </summary>
+        public static bool WriteInOneLine;
+
         #region 是否输出到控制台
+
         /// <summary>
         /// Info类型日志,输出到控制台，默认false
         /// </summary>
-        public static bool PrintInfo = false;
+        public static bool PrintInfo;
+
         /// <summary>
         /// Debug类型日志,输出到控制台，默认false
         /// </summary>
-        public static bool PrintDebug = false;
+        public static bool PrintDebug;
+
         /// <summary>
         /// Error类型日志,输出到控制台，默认false
         /// </summary>
-        public static bool PrintError = false;
+        public static bool PrintError;
+
         /// <summary>
         /// Warning类型日志,输出到控制台，默认false
         /// </summary>
-        public static bool PrintWarning = false;
+        public static bool PrintWarning;
+
         #endregion
 
         #region 是否输出到文件
+
         /// <summary>
         /// 是否输出Info日志，默认true
         /// </summary>
         public static bool WriteInfo = true;
+
         /// <summary>
         /// 是否输出Debug日志，默认true
         /// </summary>
         public static bool WriteDebug = true;
+
         /// <summary>
         /// 是否输出Error日志，默认true
         /// </summary>
         public static bool WriteError = true;
+
         /// <summary>
         /// 是否输出Warning日志，默认true
         /// </summary>
         public static bool WriteWarning = true;
+
         #endregion
 
         #region 添加Info日志
+
         /// <summary>
         /// 添加日志，保存为 Log_Info_日期
         /// </summary>
@@ -62,6 +79,7 @@ namespace LgyUtil
         {
             AddLog(strContent, "Log", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Info_日期
         /// </summary>
@@ -72,6 +90,7 @@ namespace LgyUtil
         {
             AddLog(strContent, strPerfixName, "", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Info_日期
         /// 在Log下，还可以继续创建文件夹
@@ -84,8 +103,11 @@ namespace LgyUtil
         {
             AddLog(strContent, strPerfixName, strDirName, LogLevel.Info, e);
         }
+
         #endregion
+
         #region 添加Debug日志
+
         /// <summary>
         /// 添加日志，保存为 Log_Debug_日期
         /// </summary>
@@ -95,6 +117,7 @@ namespace LgyUtil
         {
             AddDebugLog(strContent, "Log", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Debug_日期
         /// </summary>
@@ -105,6 +128,7 @@ namespace LgyUtil
         {
             AddDebugLog(strContent, strPerfixName, "", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Debug_日期
         /// 在Log下，还可以继续创建文件夹
@@ -117,8 +141,11 @@ namespace LgyUtil
         {
             AddLog(strContent, strPerfixName, strDirName, LogLevel.Debug, e);
         }
+
         #endregion
+
         #region 添加Error日志
+
         /// <summary>
         /// 添加日志，保存为 Log_Error_日期
         /// </summary>
@@ -128,6 +155,7 @@ namespace LgyUtil
         {
             AddErrorLog(strContent, "Log", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Error_日期
         /// </summary>
@@ -138,6 +166,7 @@ namespace LgyUtil
         {
             AddErrorLog(strContent, strPerfixName, "", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Error_日期
         /// 在Log下，还可以继续创建文件夹
@@ -150,8 +179,11 @@ namespace LgyUtil
         {
             AddLog(strContent, strPerfixName, strDirName, LogLevel.Error, e);
         }
+
         #endregion
+
         #region 添加Warning日志
+
         /// <summary>
         /// 添加日志，保存为 Log_Warning_日期
         /// </summary>
@@ -161,6 +193,7 @@ namespace LgyUtil
         {
             AddWarningLog(strContent, "Log", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Warning_日期
         /// </summary>
@@ -171,6 +204,7 @@ namespace LgyUtil
         {
             AddWarningLog(strContent, strPerfixName, "", e);
         }
+
         /// <summary>
         /// 添加日志，保存为 Log_Warning_日期
         /// 在Log下，还可以继续创建文件夹
@@ -183,9 +217,11 @@ namespace LgyUtil
         {
             AddLog(strContent, strPerfixName, strDirName, LogLevel.Warning, e);
         }
+
         #endregion
 
         #region 其他操作日志方法
+
         /// <summary>
         /// 启动定时删除日志，默认每天0天执行删除。
         /// 例如25日0点，保留3天，则保留22、23、24、25日，22日之前的删除
@@ -204,6 +240,7 @@ namespace LgyUtil
                 DelLog(AppDomain.CurrentDomain.BaseDirectory + "Log/", dtDelBefore);
             }, new JobOption { RunNow = true });
         }
+
         /// <summary>
         /// 删除日志
         /// </summary>
@@ -224,6 +261,7 @@ namespace LgyUtil
                     {
                         DelLog(dir);
                     }
+
                     foreach (var fPath in Directory.GetFiles(path))
                     {
                         DelLog(fPath);
@@ -242,8 +280,9 @@ namespace LgyUtil
                         AddLog($"删除日志：{path}");
                     }
                 }
-            }//删除日志的方法
+            } //删除日志的方法
         }
+
         /// <summary>
         /// 获取日志文件全路径
         /// </summary>
@@ -268,8 +307,10 @@ namespace LgyUtil
                     continue;
                 listRet.Add(f);
             }
+
             return listRet;
         }
+
         /// <summary>
         /// 根据日志路径，获取日志内容
         /// </summary>
@@ -286,9 +327,9 @@ namespace LgyUtil
             StringBuilder sb = new StringBuilder();
             logList.ForEach(s =>
             {
-                if (s.StartsWith("---2"))//每个日志开头，都是日期，匹配到日期才new对象
+                if (s.StartsWith("---2")) //每个日志开头，都是日期，匹配到日期才new对象
                 {
-                    addToList();//将上次日志结果加入集合，要开始下次的日志了
+                    addToList(); //将上次日志结果加入集合，要开始下次的日志了
                     logRet = new LogRead
                     {
                         LogDate = s.Replace("---", "").ToDateTime("yyyy-MM-dd HH:mm:ss")
@@ -311,15 +352,16 @@ namespace LgyUtil
                 }
             }
 
-            addToList();//将最后一个匹配的结果，加入集合
+            addToList(); //将最后一个匹配的结果，加入集合
             return listRet;
         }
-        #endregion
 
+        #endregion
 
         #endregion
 
         #region 日志操作，私有方法和属性
+
         /// <summary>
         /// 添加日志根方法
         /// </summary>
@@ -334,6 +376,7 @@ namespace LgyUtil
             {
                 strContent = $"错误信息：{strContent}\r\n堆栈信息：{e.Message}\r\n{e.StackTrace}";
             }
+
             strFileName = strFileName.IsNullOrEmpty() ? "Log" : strFileName + "_" + level.ToString();
             if (NeedPrint(level))
                 Console.WriteLine(strContent);
@@ -343,6 +386,7 @@ namespace LgyUtil
                 detail.AddLogQueue(strContent);
             }
         }
+
         /// <summary>
         /// 根据日志等级，获取是否打印
         /// </summary>
@@ -357,8 +401,10 @@ namespace LgyUtil
                 case LogLevel.Info: return PrintInfo;
                 case LogLevel.Warning: return PrintWarning;
             }
+
             return true;
         }
+
         /// <summary>
         /// 根据日志等级，获取是否输出到文件
         /// </summary>
@@ -373,12 +419,15 @@ namespace LgyUtil
                 case LogLevel.Info: return WriteInfo;
                 case LogLevel.Warning: return WriteWarning;
             }
+
             return true;
         }
+
         /// <summary>
         /// 每个日志文件都是一个集合，每个文件都有自己的线程存日志
         /// </summary>
         private static readonly ConcurrentDictionary<string, LogDetail> dicLogMission = new ConcurrentDictionary<string, LogDetail>();
+
         /// <summary>
         /// 日志操作详情类
         /// </summary>
@@ -388,10 +437,12 @@ namespace LgyUtil
             /// 日志文件名
             /// </summary>
             public string FileName { get; set; }
+
             /// <summary>
             /// 日志所在文件夹
             /// </summary>
             public string DirName { get; set; }
+
             /// <summary>
             /// 日志等级
             /// </summary>
@@ -403,14 +454,17 @@ namespace LgyUtil
                 DirName = strDirName;
                 Level = level;
             }
+
             /// <summary>
             /// 日志队列
             /// </summary>
             private ConcurrentQueue<LogModel> qList { get; set; } = new ConcurrentQueue<LogModel>();
+
             /// <summary>
-            /// 是否已经停止写日志
+            /// 日志线程，是否启动 1:启动中  0:停止
             /// </summary>
-            bool IsStop = true;
+            int IsRun;
+
             /// <summary>
             /// 添加到日志队列
             /// </summary>
@@ -421,41 +475,43 @@ namespace LgyUtil
                 //每次添加完，都进行日志输出
                 WriteLog();
             }
+
             /// <summary>
             /// 开始写日志
             /// </summary>
             private void WriteLog()
             {
-                if (IsStop)
+                if (Interlocked.CompareExchange(ref IsRun, 1, 0) == 0)
                 {
-                    IsStop = false;
                     if (qList.Count > 0)
                     {
                         new Thread(() =>
-                        {
-                            if (DirName.IsNotNullOrEmpty() && !DirName.EndsWith("/"))
-                                DirName += "/";
-                            var strFold = AppDomain.CurrentDomain.BaseDirectory + "Log/" + DirName;
-                            if (!Directory.Exists(strFold)) Directory.CreateDirectory(strFold);
-                            var strFile = strFold + FileName + "_" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
-                            using (var fs = new FileStream(strFile, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
                             {
-                                using (var sw = new StreamWriter(fs))
+                                if (DirName.IsNotNullOrEmpty() && !DirName.EndsWith("/"))
+                                    DirName += "/";
+                                var strFold = AppDomain.CurrentDomain.BaseDirectory + "Log/" + DirName;
+                                if (!Directory.Exists(strFold)) Directory.CreateDirectory(strFold);
+                                var strFile = strFold + FileName + "_" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                                using (var fs = new FileStream(strFile, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
                                 {
-                                    while (!qList.IsEmpty)
+                                    using (var sw = new StreamWriter(fs))
                                     {
-                                        var isHaveData = qList.TryDequeue(out var log);
-                                        if (isHaveData)
-                                            sw.Write($"---{log.HappenTime:yyyy-MM-dd HH:mm:ss}---\r\n{log.LogContent}\r\n");
+                                        while (qList.TryDequeue(out var log))
+                                        {
+                                            string oneLine = WriteInOneLine ? "\t" + log.LogContent.Replace("\n", "\t") : "\r\n" + log.LogContent;
+                                            sw.Write($"---{log.HappenTime:yyyy-MM-dd HH:mm:ss:fffff}---{oneLine}\r\n");
+                                        }
                                     }
                                 }
-                            }
-                            IsStop = true;
-                        })
-                        { IsBackground = false }.Start();//前台线程，防止进程结束，日志没记录
+
+                                //线程结束，将状态改为停止
+                                Interlocked.Exchange(ref IsRun, 0);
+                            })
+                            { IsBackground = false }.Start(); //前台线程，防止进程结束，日志没记录
                     }
                 }
             }
+
             /// <summary>
             /// 日志模型
             /// </summary>
@@ -465,14 +521,17 @@ namespace LgyUtil
                 /// 日志内容
                 /// </summary>
                 public string LogContent { get; set; }
+
                 /// <summary>
                 /// 消息发生时间
                 /// </summary>
                 public DateTime HappenTime { get; set; }
             }
         }
+
         #endregion
     }
+
     /// <summary>
     /// 日志等级
     /// </summary>
@@ -482,19 +541,23 @@ namespace LgyUtil
         /// 普通输出信息
         /// </summary>
         Info,
+
         /// <summary>
         /// 调试信息
         /// </summary>
         Debug,
+
         /// <summary>
         /// 异常信息
         /// </summary>
         Error,
+
         /// <summary>
         /// 警告
         /// </summary>
         Warning
     }
+
     /// <summary>
     /// 读取日志类
     /// </summary>
@@ -504,6 +567,7 @@ namespace LgyUtil
         /// 日志时间
         /// </summary>
         public DateTime LogDate { get; set; }
+
         /// <summary>
         /// 日志内容
         /// </summary>
